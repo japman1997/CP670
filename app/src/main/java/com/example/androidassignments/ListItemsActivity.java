d61961e40232c5abc510ca5346f451e8f9a6a47f
japman1997 <japman1997>
2019-09-19 16:23:30 -0400

    Added 3 activies for part 2

--- app/src/main/java/com/example/androidassignments/ListItemsActivity.java
+++ app/src/main/java/com/example/androidassignments/ListItemsActivity.java
@@ -5,13 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class ListItemsActivity extends AppCompatActivity {
 
-    protected static final String ACTIVITY_NAME = "StartActivity";
+    protected static final String ACTIVITY_NAME = "ListItemsActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_list_items);
         Log.i(ACTIVITY_NAME,"In onCreate()");
     }
 
